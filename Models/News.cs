c1059e56d2e29e8a11967e2980d8b38583363e94
Your Name <sagi1700@student.miun.se>
2023-06-03 11:24:17 +0200

    commit

--- Models/News.cs
+++ Models/News.cs
@@ -34,11 +34,6 @@ public class News {
     [Display(Name = "Bild")]
     public IFormFile? ImageFile {get; set;}
 
-    //public IEnumerable<Comment> Comment { get; set; } =default!;
-  //public Comment? Comment {get; set;}
-/*
-[NotMapped]
-#nullable disable */    
 public ICollection<Comment> Comments { get; set; } = new List<Comment>(); 
 }
 }
